a1ddc01ef48e28b13f27694c63cd640616cad3e1
Joel <fallendemongod@gmail.com>
2021-05-18 13:34:55 -0400

    Beginning

--- ItemSpawner/ItemSpawner.cs
+++ ItemSpawner/ItemSpawner.cs
@@ -1,7 +1,15 @@
-﻿namespace ItemSpawner
+﻿using UnityEngine;
+using ThunderRoad;
+using System.Collections.Generic;
+
+
+namespace ItemSpawner
 {
-    public class ItemSpawner
+    public class ItemSpawnerSpell : SpellCastCharge
     {
-        
+        public string itemID;
+
+        private List<string> itemIDs;
+
     }
 }
